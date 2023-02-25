5e19646bb418c8c5fbcbcc4743a74470e50883dc
Direwolf121 <jefflin1201@gmail.com>
2023-02-25 23:17:03 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const dotenv = require("dotenv").config();
+
+const request = require("request");
+const { error } = require("console");
+
+// return the index page when user accesses
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// upon connection input the socket received from client
+io.on("connection", (socket) => {
+  console.log("user has successfully connected!");
+  
+  socket.on("temperatureRequest", (cityName) => {
+    // units=metric will output temperature in celcius
+    let options = {
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.API_KEY}`,
+      method: "GET",
+      json: true,
+    }
+    
+    let temperature = null;
+    request(options, (error, res, body) => {
+      temperature = body.main.temp;
+      console.log(temperature);
+      io.emit("temperatureResponse", temperature);
+    });
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("listening on 3000");
+});
